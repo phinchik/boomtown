240a35466a16477cab30eef86ab0bb53b1357cb8
phinchik <phinchik_27@icloud.com>
2018-10-11 09:03:42 -0700

    set configuration variable

---
+++ server/config/postgres.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+module.exports = app => {
+  /**
+   * @TODO: Configuration Variables
+   *
+   *  Retrieve the necessary information to connect to Postgres
+   *  For example: app.get('PG_DB')
+   */
+  return new Pool({
+    host: app.get('PG_HOST'),
+    user: app.get('PG_USER'),
+    password: app.get('PG_PASSWORD'),
+    database: app.get('PG_DB')
+
+    /** host:
+     *  @TODO: Supply the correct configuration values to connect to postgres
+     */
+  });
+};
